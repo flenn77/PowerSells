4215a115024773642ada6371693dcaf4cdb443b4
mouradof <mouradhadjresp@gmail.com>
2023-10-25 22:43:49 +0200

    add security for access

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,10 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
+    /**
+     * @Security("is_granted('ROLE_CAISSIER') or is_granted('ROLE_MANAGER')")
+     */
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [
--- src/Controller/InventoryController.php
+++ src/Controller/InventoryController.php
@@ -5,10 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class InventoryController extends AbstractController
 {
     #[Route('/inventory', name: 'app_inventory')]
+    /**
+     * @Security("is_granted('ROLE_CAISSIER') or is_granted('ROLE_MANAGER')")
+     */
     public function index(): Response
     {
         return $this->render('inventory/index.html.twig', [
@@ -17,6 +21,9 @@ class InventoryController extends AbstractController
     }
 
     #[Route('/inventory/form', name: 'app_inventory_form')]
+    /**
+     * @Security("is_granted('ROLE_CAISSIER') or is_granted('ROLE_MANAGER')")
+     */
     public function form(): Response
     {
         return $this->render('inventory/formInventory.html.twig', [
